0a5d2591afcb8dd235975547967d3a5b18973ec9
Vishwas987 <vishwas.bhaskar@comprotechnologies.com>
2022-10-03 11:48:57 +0530

    css BEM class_name changes

--- login.js
+++ login.js
@@ -1,19 +1,19 @@
 function login(){
-    var u_name = document.getElementById("email").value;
+    var uName = document.getElementById("email").value;
     var pwd = document.getElementById("password").value;
     var msg = document.getElementById("message");
-    var msg_box = document.getElementById("message_box");
+    var msgBox = document.getElementById("message_box");
 
-    if(u_name == '' || pwd == ''){
-        msg_box = msg_box.className = "msg__box msg__box--wrong";
+    if(uName == '' || pwd == ''){
+        msgBox = msgBox.className = "msg-box msg-box--wrong";
         msg = msg.innerText = "Please enter email and password";
     }
-    else if(u_name == 'admin' && pwd == 'admin123'){
-        msg_box = msg_box.className = "msg__box msg__box--correct";
+    else if(uName == 'admin' && pwd == 'admin123'){
+        msgBox = msgBox.className = "msg-box msg-box--correct";
         msg = msg.innerText = "Login Success!";
     }
     else {
-        msg_box = msg_box.className = "msg__box msg__box--wrong";
+        msgBox = msgBox.className = "msg-box msg-box--wrong";
         msg = msg.innerText = "Please enter correct email and password";
     }
     
